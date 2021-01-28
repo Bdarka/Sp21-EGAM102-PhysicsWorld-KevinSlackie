6fc38ace0d1830c00605136c09f4c856a601af9a
Bdarka <70989149+Bdarka@users.noreply.github.com>
2021-01-27 20:41:27 -0800

    Part 2 Start

---
+++ Spooky Physics/Assets/Scripts/Loops.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string outputString = "";
+
+        for(int i = 1; i < 101; i++)
+        {
+            outputString += i.ToString() + ", ";
+        }
+
+        Debug.Log(outputString);
+
+;    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Spooky Physics/Assets/Scripts/PatternMakerUniformRandomDistribution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternMakerUniformRandomDistribution : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public int numOfClones;
+
+    public float xMin, xMax, yMin, yMax;
+    public float cloneScaleMin, cloneScaleMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObject; 
+
+        for(int i = 0; i < numOfClones; i++)
+        {
+            newObject = Instantiate(objectToClone, this.transform);
+
+            newObject.transform.position = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0);
+
+            newObject.transform.localScale = Random.Range(cloneScaleMin, cloneScaleMax) * Vector3.one;
+
+        }
+    }
+
+}
